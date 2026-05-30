6a13d5fc0ca599ee6311e083c921f509255b6336
Taylorss19 <dy.avalos@duocuc.cl>
2026-05-30 11:33:40 -0400

    funciones en otro archivo

---
+++ Funciones/FuncionesDetalle.py
@@ -0,0 +1,13 @@
+#funciones
+def SumarDosNumeros():
+    '''Esta funcion permite sumar dos números,
+        ingresados dentro de la función    
+        '''
+    num1=int(input("Ingrese número 1: "))
+    num2=int(input("Ingrese número 2: "))
+    suma=num1+num2
+    print(f"la suma del {num1} + {num2} es: {suma}")
+    
+def sumar(a,b):
+    suma= a + b
+    return suma
